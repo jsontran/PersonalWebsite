e46bec05c75ea54f3a97e426012c9ae384ca927d
Jason Tran <jjsontran@gmail.com>
2021-10-22 01:39:41 -0400

    Formatted the About Section.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,15 +10,31 @@ export default function About() {
                 <div className="left">
                     <img src="./Assets/bot.jpg" className="botImg" alt="" />
                     <img src="./Assets/pfp.jpg" className="topImg" alt="" />
+                    <div className="title">
+                        ABOUT.ME
+                    </div>
                 </div>
                 <div className="right">
-                    HELLo
+                    <h4>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                        officia deserunt mollit anim id est laborum.
+
+                    </h4>
+                    <h4>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                        ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                        mollit anim id est laborum.
+                    </h4>
                 </div>
             </div>
 
-            <div className="title">
-                ABOUT ME
-            </div>
+
         </div>
 
     )
