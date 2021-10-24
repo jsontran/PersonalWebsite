8c67509acfee75e8b3183d35383c0113a6be3148
Jason Tran <jjsontran@gmail.com>
2021-10-24 19:18:19 -0400

    Added Home Animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,19 +1,26 @@
 import "./intro.scss"
 import { useEffect, useRef } from "react"
+import Fade from 'react-reveal/Fade';
 
 export default function Intro() {
     return (
         <div className="Intro" id="Intro">
             <div className="wrapper">
-                <div className="logo">
-                    J.T
-                </div>
-                <div className="top">
-                    JASON
-                </div>
-                <div className="bot">
-                    TRAN
-                </div>
+                <Fade delay={2000}>
+                    <div className="logo" >
+                        J.T
+                    </div>
+                </Fade>
+                <Fade bottom duration={2000}>
+                    <div className="top">
+                        JASON
+                    </div>
+                </Fade>
+                <Fade top duration={2000}>
+                    <div className="bot">
+                        TRAN
+                    </div>
+                </Fade>
             </div>
         </div>
     )
