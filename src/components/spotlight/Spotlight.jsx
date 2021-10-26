014e997c2509380949ea2def8a1e43f1c1e278e8
Jason Tran <jjsontran@gmail.com>
2021-10-25 21:11:49 -0400

    Formmatted the Experience page.

--- src/components/spotlight/Spotlight.jsx
+++ src/components/spotlight/Spotlight.jsx
@@ -4,40 +4,27 @@ export default function Spotlight() {
     return (
         <div className="Spotlight" id="Spotlight">
             <div className="space"> hello</div>
-            <h1> Spotlight</h1>
+            <h1> Experience</h1>
             <div className="container">
                 <div className="card">
-                    <div className="top">
+                    <div className="left">
                         <img src="" className="left" alt="" />
-                        <img src="" className="user" alt="" />
-                        <img src="" className="right" alt="" />
                     </div>
 
-                    <div className="center">21803948 092183049 801239 84091238 90481230948 09238 409128304 98012394 8091238904</div>
-                    <div className="bottom">TITLE TITLE TITLE</div>
-
+                    <div className="right">
+                        <div className="bottom">TITLE TITLE TITLE</div>
+                        <div className="center">21803948 092183049 801239 84091238 90481230948 09238 409128304 98012394 8091238904</div>
+                    </div>
                 </div>
                 <div className="card">
-                    <div className="top">
+                    <div className="left">
                         <img src="" className="left" alt="" />
-                        <img src="" className="user" alt="" />
-                        <img src="" className="right" alt="" />
                     </div>
 
-                    <div className="center">21803948 092183049 801239 84091238 90481230948 09238 409128304 98012394 8091238904</div>
-                    <div className="bottom">TITLE TITLE TITLE</div>
-
-                </div>
-                <div className="card">
-                    <div className="top">
-                        <img src="" className="left" alt="" />
-                        <img src="" className="user" alt="" />
-                        <img src="" className="right" alt="" />
+                    <div className="right">
+                        <div className="bottom">TITLE TITLE TITLE</div>
+                        <div className="center">21803948 092183049 801239 84091238 90481230948 09238 409128304 98012394 8091238904</div>
                     </div>
-
-                    <div className="center">21803948 092183049 801239 84091238 90481230948 09238 409128304 98012394 8091238904</div>
-                    <div className="bottom">TITLE TITLE TITLE</div>
-
                 </div>
             </div>
         </div>
