45b530732da3f033b415963ac4bf86fca90e1306
Jason Tran <jjsontran@gmail.com>
2021-10-29 17:19:36 -0400

    Fixed experience container width bug.

--- src/components/spotlight/Spotlight.jsx
+++ src/components/spotlight/Spotlight.jsx
@@ -3,28 +3,53 @@ import "./spotlight.scss"
 export default function Spotlight() {
     return (
         <div className="Spotlight" id="Spotlight">
-            <h1> Experience</h1>
+            <h1> EXPERIENCE</h1>
             <div className="container">
                 <div className="card">
                     <div className="left">
                         <img src="" className="left" alt="" />
                     </div>
-
+                    <div className="center">
+                        <div>TITLE TITLE TITLE</div>
+                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                            officia deserunt mollit anim id est laborum.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                            fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                            mollit anim id est laborum.</div>
+                    </div>
                     <div className="right">
-                        <div className="bottom">TITLE TITLE TITLE</div>
-                        <div className="center">21803948 092183049 801239 84091238 90481230948 09238 409128304 98012394 8091238904</div>
+                        <img src="" className="right" alt="" />
                     </div>
                 </div>
                 <div className="card">
                     <div className="left">
                         <img src="" className="left" alt="" />
                     </div>
-
+                    <div className="center">
+                        <div>TITLE TITLE TITLE</div>
+                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                            officia deserunt mollit anim id est laborum.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                            fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                            mollit anim id est laborum.</div>
+                    </div>
                     <div className="right">
-                        <div className="bottom">TITLE TITLE TITLE</div>
-                        <div className="center">21803948 092183049 801239 84091238 90481230948 09238 409128304 98012394 8091238904</div>
+                        <img src="" className="right" alt="" />
                     </div>
                 </div>
+
             </div>
         </div>
     )
