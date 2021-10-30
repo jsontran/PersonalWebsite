e7f2f374195b496612f748a58634b0be81689bd2
Jason Tran <jjsontran@gmail.com>
2021-10-30 00:49:05 -0400

    Formatted Experience Section

--- src/components/spotlight/Spotlight.jsx
+++ src/components/spotlight/Spotlight.jsx
@@ -6,10 +6,8 @@ export default function Spotlight() {
             <h1> EXPERIENCE</h1>
             <div className="container">
                 <div className="card">
-                    <div className="left">
-                        <img src="" className="left" alt="" />
-                    </div>
-                    <div className="center">
+                    <img src="" className="logo" alt="" />
+                    <div className="text">
                         <div>TITLE TITLE TITLE</div>
                         <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -21,17 +19,14 @@ export default function Spotlight() {
                             dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                             ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                             fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim id est laborum.</div>
+                            mollit anim id est laborum.
+                        </div>
                     </div>
-                    <div className="right">
-                        <img src="" className="right" alt="" />
-                    </div>
+                    <img src="./Assets/exp1.jpg" className="img" alt="" />
                 </div>
                 <div className="card">
-                    <div className="left">
-                        <img src="" className="left" alt="" />
-                    </div>
-                    <div className="center">
+                    <img src="" className="logo" alt="" />
+                    <div className="text">
                         <div>TITLE TITLE TITLE</div>
                         <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -43,12 +38,12 @@ export default function Spotlight() {
                             dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                             ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                             fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim id est laborum.</div>
+                            mollit anim id est laborum.
+                        </div>
                     </div>
-                    <div className="right">
-                        <img src="" className="right" alt="" />
-                    </div>
+                    <img src="./Assets/exp1.jpg" className="img" alt="" />
                 </div>
+
 
             </div>
         </div>
