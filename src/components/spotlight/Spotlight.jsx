4e02a2cf4bcdfa2d6d986616fbbad5b39b5af004
Jason Tran <jjsontran@gmail.com>
2021-10-30 15:41:42 -0400

    Added Animation

--- src/components/spotlight/Spotlight.jsx
+++ src/components/spotlight/Spotlight.jsx
@@ -1,51 +1,57 @@
 import "./spotlight.scss"
+import Fade from 'react-reveal/Fade';
 
 export default function Spotlight() {
     return (
         <div className="Spotlight" id="Spotlight">
             <h1> EXPERIENCE</h1>
             <div className="container">
-                <div className="cardE">
-                    <img src="" className="logoE" alt="" />
 
-                    <div className="textE">
-                        <div className="TitleE">Research Assistant</div>
-                        <div className="sTitleE">McMaster University | May 2021 - Aug 2021</div>
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                            officia deserunt mollit anim id est laborum.
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                            fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim id est laborum.
+                <Fade left>
+                    <div className="cardE">
+                        <img src="" className="logoE" alt="" />
+                        <div className="textE">
+                            <div className="TitleE">Research Assistant</div>
+                            <div className="sTitleE">McMaster University | May 2021 - Aug 2021</div>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                officia deserunt mollit anim id est laborum.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                                dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                                ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                                fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                                mollit anim id est laborum.
+                            </div>
                         </div>
+                        <img src="./Assets/exp1.jpg" className="imgE" alt="" />
                     </div>
-                    <img src="./Assets/exp1.jpg" className="imgE" alt="" />
-                </div>
-                <div className="cardO">
-                    <img src="" className="logoO" alt="" />
-                    <div className="textO">
-                        <div className="TitleO">Research Assistant</div>
-                        <div className="sTitleO">McMaster University | May 2021 - Aug 2021</div>
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                            officia deserunt mollit anim id est laborum.
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                            fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim id est laborum.
+                </Fade>
+
+                <Fade right>
+                    <div className="cardO">
+                        <img src="" className="logoO" alt="" />
+                        <div className="textO">
+                            <div className="TitleO">Research Assistant</div>
+                            <div className="sTitleO">McMaster University | May 2021 - Aug 2021</div>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                officia deserunt mollit anim id est laborum.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                                dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                                ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                                fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                                mollit anim id est laborum.
+                            </div>
                         </div>
+                        <img src="./Assets/exp2.jpg" className="imgO" alt="" />
                     </div>
-                    <img src="./Assets/exp2.jpg" className="imgO" alt="" />
-                </div>
+                </Fade>
 
 
             </div>
